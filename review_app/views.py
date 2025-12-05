a7234dd6fea450bb514aa0de7cd06099f29cc4bd
ShutenkoStas <shotenko494@gmail.com>
2025-12-06 00:18:55 +0200

    Add view for review_list, create templates index.html and base.html add urls for home page add templates to setting

--- review_app/views.py
+++ review_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from review_app.models import *
+
+def index(request):
+    reviews = Review.objects.all()
+    context = {
+        "reviews" : reviews
+    }
+    return render(request,
+                   template_name="reviews/index.html",
+                   context=context)
 
 # Create your views here.
