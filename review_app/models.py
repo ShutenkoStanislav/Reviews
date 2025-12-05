c0fc1027cb45bc98b13644fda85909c04ed580b3
ShutenkoStas <shotenko494@gmail.com>
2025-12-05 23:39:39 +0200

    Add model Review and add urls.py for app file

--- review_app/models.py
+++ review_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Review(models.Model):
+    username = models.CharField(max_length=256)
+    text = models.TextField()
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.username} - {self.date}"
+    class Meta:
+        verbose_name = "Відгук"
+        verbose_name_plural = "Відгуки"
+        ordering = "-date"
+
